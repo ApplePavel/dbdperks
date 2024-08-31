a3cf41cf3ab2ee5e61166e3e1c1468fcafbe4a3b
Pavel Seduan <sphreofdie@gmail.com>
2024-08-31 18:23:26 +0300

    v3

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import styles from '../styles/LeftMenu.module.css'
+import React, { useState } from 'react';
+import styles from '../styles/LeftMenu.module.css';
 
 function LeftMenu() {
+  const [isKillerActive, setIsKillerActive] = useState(true); // Состояние для переключателя
+
+  const toggleMenu = () => {
+    setIsKillerActive(!isKillerActive); // Переключение состояния
+  };
+
   return (
     <div className={styles.left_menu}>
-        <div className={styles.menu_profile}></div>
-        <div className={styles.menu_perks}></div>
-        <div className={styles.menu_switch}></div>
+      {isKillerActive ? (
+        <div className={styles.menu_killer} onClick={toggleMenu}></div>
+      ) : (
+        <div className={styles.menu_survivor} onClick={toggleMenu}></div>
+      )}
     </div>
-
-  )
+  );
 }
 
-export default LeftMenu
+export default LeftMenu;
